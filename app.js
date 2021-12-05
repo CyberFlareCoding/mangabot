3f5cd956fa6cd2523fbad698cc0bc34cec9d2cba
j g <soniccellgaming@gmail.com>
2021-12-05 13:44:27 -0500

    3rd try

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/',function(req, res) {
-    res.sendFile(__dirname + './index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(port, function () {
